4d5777e6979000d4e89673b282741a6168482b10
yashvasudeva1 <117675995+yashvasudeva1@users.noreply.github.com>
2025-10-27 14:36:51 +0530

    Update and rename app2.py to app.py

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-Python 3.11.9 (tags/v3.11.9:de54cf5, Apr  2 2024, 10:12:12) [MSC v.1938 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
 import streamlit as st 
 import numpy as np 
 import pandas as pd
@@ -95,3 +93,4 @@ with tab5:
 ...     user_input_df = user_input_df[X_train.columns]
 ...     if st.button("Predict Addicted Score"):
 ...         prediction = rf_classifier.predict(user_input_df)
+
